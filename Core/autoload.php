5b4a677834b1d63f810a032da85727701494aabb
noam.dst <dos-santos.noam@epitech.eu>
2020-03-24 17:05:54 +0100

    autoloader

--- Core/autoload.php
+++ Core/autoload.php
@@ -1,1 +1,11 @@
 <?php
+spl_autoload_register(function ($class) {
+    $path = substr(__DIR__, 0, -strlen(basename(__DIR__)));
+    $ext = '.php';
+    $fullpath = $path . $class . $ext;
+    if (!file_exists($fullpath)) {
+        $path .= 'src\\';
+        $fullpath = $path . $class . $ext;
+    }
+    require_once $fullpath;
+});
