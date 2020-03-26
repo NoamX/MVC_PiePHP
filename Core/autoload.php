2422aac847455a826a6e01389ba3fcda23944e41
noam.dst <dos-santos.noam@epitech.eu>
2020-03-26 15:36:41 +0100

    Correction autoloader

--- Core/autoload.php
+++ Core/autoload.php
@@ -1,11 +1,10 @@
 <?php
 spl_autoload_register(function ($class) {
-    $path = '.' . DIRECTORY_SEPARATOR;
-    $ext = '.php';
-    $fullpath = $path . $class . $ext;
-    if (!file_exists($fullpath)) {
-        $path .= 'src' . DIRECTORY_SEPARATOR;
-        $fullpath = $path . $class . $ext;
+    $paths = ['.' . DIRECTORY_SEPARATOR . 'Core', '.' . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Controller', '.' . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Model'];
+    foreach ($paths as $value) {
+        $fullpath = $value . strstr($class, DIRECTORY_SEPARATOR) . '.php';
+        if (file_exists($fullpath)) {
+            require_once($fullpath);
+        }
     }
-    require_once $fullpath;
 });
