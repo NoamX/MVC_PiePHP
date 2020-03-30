034c462079498a65756b2801b685072234f03e11
noam.dst <dos-santos.noam@epitech.eu>
2020-03-30 16:59:32 +0200

    je reflechis pour le render

--- src/Model/UserModel.php
+++ src/Model/UserModel.php
@@ -7,16 +7,15 @@ class UserModel
     private static $db;
     public static function connectPDO()
     {
-        return self::$db = new \PDO('mysql:host=localhost;dbname=MVC_PiePHP;', 'root', '');
+        self::$db = new \PDO('mysql:host=localhost;dbname=MVC_PiePHP;', 'root', '');
     }
 
     public static function readAll()
     {
-
         $req = self::$db->query('SELECT * FROM users');
         $res = $req->fetchAll(5);
         foreach ($res as $value) {
-            echo "$value->email<br>";
+            echo "<p>$value->email</p>";
         }
     }
 }
